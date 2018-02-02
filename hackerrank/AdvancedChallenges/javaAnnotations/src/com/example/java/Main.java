83cfa5355335af0412422b94c9a2e59db68a51c5
Dmitry <kropotovd@gmail.com>
2018-02-02 23:28:26 +0300

    task Java Annotations from Advanced Challanges on hackerrank. Hackerrank did not accept this decision, but it seems to me that this is hackerrank mistake

---
+++ hackerrank/AdvancedChallenges/javaAnnotations/src/com/example/java/Main.java
@@ -0,0 +1,67 @@
+package com.example.java;
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.util.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface FamilyBudget {
+    String userRole() default "GUEST";
+    int budgetLimit();//~~Complete the interface~~
+}
+
+class FamilyMember {
+    //~~Complete this line~~
+    @FamilyBudget(userRole = "SENIOR", budgetLimit = 100)
+    public void seniorMember(int budget, int moneySpend) {
+        System.out.println("Senior Member");
+        System.out.println("Spend: " + moneySpend);
+        System.out.println("Budget Left: " + (budget - moneySpend));
+    }
+
+    //~~Complete this line~~
+    @FamilyBudget(userRole = "JUNIOR", budgetLimit = 50)
+    public void juniorUser(int budget, int moneySpend) {
+        System.out.println("Junior Member");
+        System.out.println("Spend: " + moneySpend);
+        System.out.println("Budget Left: " + (budget - moneySpend));
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String role = in.next();
+            int spend = in.nextInt();
+            try {
+                Class annotatedClass = FamilyMember.class;
+                Method[] methods = annotatedClass.getMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(FamilyBudget.class)) {
+                        FamilyBudget family = method
+                                .getAnnotation(FamilyBudget.class);
+                        String userRole = family.userRole();
+                        int budgetLimit = family.budgetLimit();//~~Complete this line~~;
+                        if (userRole.equals(role)) {
+                            if(spend<=budgetLimit/*~~Complete this line~~*/){
+                                method.invoke(FamilyMember.class.newInstance(),
+                                        budgetLimit, spend);
+                            }else{
+                                System.out.println("Budget Limit Over");
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            testCases--;
+        }
+    }
+}
